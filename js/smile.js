cdf725fef6c049b45e306384e7abe573b470ebb6
ifzm <214123975@qq.com>
2016-01-28 15:25:39 +0800

    Site updated: 2016-01-28 15:25:37

--- js/smile.js
+++ js/smile.js
@@ -38,13 +38,13 @@ function addCount(Counter) {
     query.find({
         success: function(results) {
             if (results.length > 0) {
-                var counter = results[0];
-                counter.fetchWhenSave(true);
-                counter.increment("time");
+                var counter = results[0]
+                counter.fetchWhenSave(true)
+                counter.increment("time")
                 counter.save(null, {
                     success: function(counter) {
-                        var $element = document.getElementById(url);
-                        $element.lastChild.innerHTML = counter.get('time');
+                        var $element = document.getElementById(url)
+                        $element.lastChild.innerHTML = counter.get('time')
                     },
                     error: function(counter, error) {
                         console.log(
@@ -53,14 +53,14 @@ function addCount(Counter) {
                     }
                 })
             } else {
-                var newcounter = new Counter();
-                newcounter.set("title", title);
-                newcounter.set("url", url);
-                newcounter.set("time", 1);
+                var newcounter = new Counter()
+                newcounter.set("title", title)
+                newcounter.set("url", url)
+                newcounter.set("time", 1)
                 newcounter.save(null, {
                     success: function(newcounter) {
-                        var $element = document.getElementById(url);
-                        $element.lastChild.innerHTML = newcounter.get('time');
+                        var $element = document.getElementById(url)
+                        $element.lastChild.innerHTML = newcounter.get('time')
                     },
                     error: function(newcounter, error) {
                         console.log('Failed to create')
@@ -69,11 +69,12 @@ function addCount(Counter) {
             }
         },
         error: function(error) {
-            console.log('Error:' + error.code + " " + error.message);
+            console.log('Error:' + error.code + " " + error.message)
         }
     })
 }
 $(function() {
+    // lazyload
     if ($.lazyload === true)
         $("img.lazy").lazyload({
             data_attribute: "src",
@@ -82,4 +83,42 @@ $(function() {
     // initialize
     var Counter = AV.Object.extend("Counter")
     $('.visitors').length === 1 ? addCount(Counter) : showAll(Counter)
+
+    //Back-To-Top
+    if ($(window).scrollTop() !== 0){
+        $('.back-to-top').stop().fadeIn()
+    }
+    $(window).scroll(function() {
+        /* Act on the event */
+        if ($(this).scrollTop() === 0){
+            $('.back-to-top').stop().fadeOut()
+        }
+        if ($(this).scrollTop() !== 0){
+            $('.back-to-top').stop().fadeIn()
+        }
+    })
+    // click
+    $('.back-to-top').click(function(){
+        $("html,body").stop().animate({scrollTop:"0px"})
+    })
+    $('.show-menu').click(function(){
+        $(this).off('hover').removeClass('hover')
+        $('.show-menu, .menu-container').toggleClass('open')
+        $('body').toggleClass('scroll open')
+
+        var bodyWidth = $('body').width(),
+            movePlus = $('.menu-container').width() + $('.menu-container').width()/2
+        if($('body').is('.open') && bodyWidth > movePlus){
+            $('body').css('left', -(bodyWidth - movePlus)/2)
+        }else{
+             $('body').css('left', 0)
+        }
+        if($(window).scrollTop() !== 0){
+            $('.back-to-top').stop().fadeToggle()
+        }
+    }).hover(function(){
+        $(this).addClass('hover')
+    }, function(){
+        $(this).removeClass('hover')
+    })
 })
